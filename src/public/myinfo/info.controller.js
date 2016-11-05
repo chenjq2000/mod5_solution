8fad49d6126b711f408ddd66b044e40b86ced358
Jim Chen <chenjq2000@yahoo.com>
2016-11-05 18:46:11 -0400

    new changes

--- src/public/myinfo/info.controller.js
+++ src/public/myinfo/info.controller.js
@@ -11,13 +11,13 @@ InfoController.$inject = ['$rootScope','$state','ApiPath', 'UserInfoService'];
 function InfoController($rootScope,$state,ApiPath, UserInfoService) {
   var $ctrl = this;
   $ctrl.basePath = ApiPath;
-  // $ctrl.user = UserInfoService.getUser();
-  //
-  // $ctrl.$onInit = function($state) {
-  //   if (!UserInfoService.isRegistered()) {
-  //     $state.go('public.registration');
-  //   }
-  // }
+  $ctrl.user = UserInfoService.getUser();
+
+  $ctrl.$onInit = function($state) {
+    if (!UserInfoService.isRegistered()) {
+      $state.go('public.registration');
+    }
+  }
 
 }
 
